a6aaf60272678078dd2b84fc8a9ea72c304750bf
ThibautMilville <tmilville.pro@gmail.com>
2023-05-10 22:10:49 +0200

    bases

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Stripe checkout integration</title>
+  <link rel="stylesheet" href="style/style.css">
+</head>
+<body>
+  <div class="container">
+    <h1>Stripe checkout Integration</h1>
+    <div class="item">
+      
+    </div>
+  </div>
+</body>
+</html>
