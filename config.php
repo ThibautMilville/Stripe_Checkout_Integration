13a9527a662ffed8a197299cf52c611a0dfc9dcb
ThibautMilville <tmilville.pro@gmail.com>
2023-05-14 17:42:03 +0200

    secret and publishable keys changed

--- config.php
+++ config.php
@@ -6,8 +6,8 @@
   $currency = "usd";
 
   // STRIPE API CONFIGURATION (SWITCH TO PRODUCTION API KEYS FOR REAL TRANSACTIONS)
-  define('STRIPE_API_KEY', 'sk_test_51N6GaFDobsEbj3fA0yJ3c3Z98s0JNNs2pSthQL0AiPshjHSdSH35Qo6BQrujOCfet0RceFCqAy85DIL3Q2MyCUtH003XYWak2R');
-  define('STRIPE_PUBLISHABLE_KEY', 'pk_test_51N6GaFDobsEbj3fA6bXlDS6EdFve4jhh5A52BF8QZrjqdAiH3pMBp3GQ05k7hoPMjt3hz078FrsNuM3tQEntbyz0003avTi9XX');
+  define('STRIPE_API_KEY', 'PUT_YOUR_SECRET_KEY_HERE');
+  define('STRIPE_PUBLISHABLE_KEY', 'PUT_YOUR_PUBLISHABLE_KEY_HERE');
   define('STRIPE_SUCCESS_URL', 'http://localhost:8888/projets/stripe_checkout_integration/payment-success.php');
   define('STRIPE_CANCEL_URL', 'http://localhost:8888/projets/stripe_checkout_integration/payment-cancel.php');
 
